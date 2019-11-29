65df98d902f493ad74282f3ee43b7e3e4af7f04b
George <lanreadedara@MACs-MacBook-Pro-6.local>
2019-11-29 16:23:46 +0100

    second commit

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
         'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, COntent-Type, Accept, Authorization'
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
